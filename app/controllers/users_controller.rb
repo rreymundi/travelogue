21c9593c031f85bcbd8751656b3b44b4042f5acd
Raúl Reymundi <raul@kickstarter.com>
2023-04-07 17:35:54 -0400

    add index action to user controller for testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        render json: users, status: :ok
+      end
+
     def create
         user = User.create!(user_params)
         render json: user, status: :created
