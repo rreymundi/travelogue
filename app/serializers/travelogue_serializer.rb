1d8bf2d37cc2ef7ec934da881df68e42580fc1d3
Raúl Reymundi <raul@kickstarter.com>
2023-04-26 18:41:46 -0400

    update travelogue serializer

--- app/serializers/travelogue_serializer.rb
+++ app/serializers/travelogue_serializer.rb
@@ -1,3 +1,4 @@
 class TravelogueSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :location, :cover_image_url, :created_at, :tags
+  attributes :id, :title, :description, :location, :cover_image_url, :created_at
+  has_many :tags, through: :post_tags
 end
