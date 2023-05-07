0a46b6b466947a7eda35e696c192e342eb208c9e
Raúl Reymundi <raul@kickstarter.com>
2023-05-07 13:44:57 -0400

    add rendering of bookmarked Travelogues in the Bookmarks component/page

--- client/src/pages/Bookmarks.js
+++ client/src/pages/Bookmarks.js
@@ -1,23 +1,48 @@
 import React, { useState, useContext } from 'react';
 import { UserContext } from '../context/user';
-import { Box, Typography } from '@mui/material'
+import { 
+  Box, 
+  Grid,
+  Typography
+ } from '@mui/material';
+ import TravelogueCard from '../components/TravelogueCard';
 
-const Bookmarks = () => {
+const Bookmarks = ({ onBookmarkSave, onBookmarkUnsave }) => {
   const {user} = useContext(UserContext);
 
   const boxStyle = {
     backgroundColor: '#F7F7F6',
     padding: '3rem',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-}
+    display: 'grid'
+  };
+
+  const savedTravelogues = user?.travelogues?.filter(
+    travelogue => user.saved_posts.find( savedPost => savedPost.travelogue_id === travelogue.id)
+  );
+  console.log(savedTravelogues);
 
   return (
     <Box sx={boxStyle}>
-      <Typography sx={{ fontSize: '3.5rem', m: '1rem'}}>Bookmarks</Typography>
-    </Box>
+    <Box>
+        <Box >
+            <Typography sx={{ fontSize: '3.5rem' }}>Bookmarks</Typography>
+            <Typography sx={{ fontSize: '1.5rem'}}>
+                Browse through your saved travelogues
+            </Typography>
+            {user.saved_posts 
+            ? 
+            <Box sx={{ margin: '2.5rem'}}>
+                <Grid container spacing={2}>
+                    {savedTravelogues.map((travelogue) => (
+                        <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
+                    ))}
+                </Grid>
+            </Box>
+            : null
+            }
+        </Box>
+    </Box>      
+  </Box>
   )
 }
 
