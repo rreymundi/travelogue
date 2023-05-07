275091ca5ce2b7acf758fd002bd687637f0446fe
Raúl Reymundi <raul@kickstarter.com>
2023-05-07 13:42:26 -0400

    add saved_posts actions that correspond to the post and delete in saved_posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   post '/profile/avatar', to: "users#avatarchange"
   post '/travelogue/:id/cover', to: "travelogues#coverimagechange"
 
+  post '/bookmarks', to: "saved_posts#create"
+  delete '/bookmarks/:id', to: "saved_posts#destroy"
+
   resources :travelogues, only: [:index, :show, :create, :update, :destroy]
-  resources :collections, only: [:index]
   # these will need to be removed later
   resources :users, only: [:index, :create, :update]
   resources :tags, only: [:index, :destroy]
