e3dfee81927f4321fd7fa5f7742c86bb7c2fb554
Raúl Reymundi <raul@kickstarter.com>
2023-05-07 14:06:33 -0400

    update travelogue serializer to include user.username

--- app/serializers/travelogue_serializer.rb
+++ app/serializers/travelogue_serializer.rb
@@ -1,4 +1,5 @@
 class TravelogueSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :location, :cover_image_url, :created_at, :tags
   has_many :tags, through: :post_tags
+  belongs_to :user
 end
