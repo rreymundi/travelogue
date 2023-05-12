288815d12caaf36a6d56659b7a619185c7676d46
Raúl Reymundi <raul@kickstarter.com>
2023-05-12 14:22:28 -0400

    clean up some unused code in the Discover component

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -7,10 +7,9 @@ import TravelogueCard from '../components/TravelogueCard';
 import { ErrorContext } from '../context/error';
 import { useLocation } from 'react-router-dom';
 
-const Discover = ({ allTravelogues, onBookmarkSave, onBookmarkUnsave, searchedTravelogues }) => {
-    const {setErrors} = useContext(ErrorContext);
+const Discover = ({ allTravelogues, onBookmarkSave, onBookmarkUnsave }) => {
+    const {errors, setErrors} = useContext(ErrorContext);
     const [isLoading, setIsLoading] = useState(false);
-    const [isMounted, setIsMounted] = useState(false);
     const [searchResults, setSearchResults] = useState([]);
 
     const location = useLocation();
@@ -20,18 +19,20 @@ const Discover = ({ allTravelogues, onBookmarkSave, onBookmarkUnsave, searchedTr
     // it then sends a GET request to the "query" from the '/discover/search/:query' route
     // which corresponds to the "travelogues#search" action in the travelogues controller
     useEffect(() => {
+        setIsLoading(true);
         fetch(`/discover/search/${new URLSearchParams(location.search).get('query')}`)
         .then((r) => {
           if (r.ok) {
-            r.json().then((r) => setSearchResults(r))
+            r.json().then((data) => setSearchResults(data))
           } else {
-            r.json().then((r) => setErrors(r.errors))
+            r.json().then((data) => setErrors(data.errors))
           }
         })
+        setIsLoading(false)
     }, []);
-    
+
     // conditional rendering of the travelogue cards
-    // if the searchResults array is empty (i.e. the user has not searched for anything or
+    // if the searchResults array is null (i.e. the user has not searched for anything or
     // attempted to search an empty string), then the allTravelogues array is mapped over
     const renderedResults = searchResults.length > 0
     ?   <Grid container spacing={2}>
@@ -39,11 +40,33 @@ const Discover = ({ allTravelogues, onBookmarkSave, onBookmarkUnsave, searchedTr
                 <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
             ))}
         </Grid>
-    :   <Grid container spacing={2}>
-            {allTravelogues?.map((travelogue) => (
-                <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
-            ))}
+    :   <>
+        <Grid container spacing={2}>
+          {allTravelogues?.map((travelogue) => (
+            <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
+          ))}
         </Grid>
+        </>
+
+    // const rendered = () => {
+    //   if (searchResults.length === 0) {
+    //     return <Typography>No results</Typography>
+    //   } else if (searchResults.length > 0) {
+    //     return <Grid container spacing={2}>
+    //               {searchResults?.map((travelogue) => (
+    //                 <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
+    //               ))}
+    //             </Grid>
+    //     } else {
+    //       return <Grid container spacing={2}>
+    //                 {allTravelogues?.map((travelogue) => (
+    //                   <TravelogueCard item key={travelogue.id} travelogue={travelogue} onBookmarkSave={onBookmarkSave} onBookmarkUnsave={onBookmarkUnsave} />
+    //                 ))}
+    //               </Grid>
+    //     }
+    // };
+
+    if (isLoading) return <p>Loading...</p>
 
     return (
       <Box sx={{
@@ -59,7 +82,9 @@ const Discover = ({ allTravelogues, onBookmarkSave, onBookmarkUnsave, searchedTr
                     Browse through the latest travelogues
                 </Typography>
                 <Box sx={{ margin: '2.5rem'}}>
+                  <Grid container spacing={2}>
                     {renderedResults}
+                  </Grid>
                 </Box>
             </Box>
         </Box>      
