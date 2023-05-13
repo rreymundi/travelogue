0d022ea81151e33ae18f1e7ea6a76881d79c1103
Raúl Reymundi <raul@kickstarter.com>
2023-05-13 13:31:53 -0400

    fix user menu anchor bug

--- client/src/components/ResponsiveAppBar.js
+++ client/src/components/ResponsiveAppBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { UserContext } from '../context/user';
 import { ErrorContext } from '../context/error';
@@ -15,22 +15,15 @@ import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import UserMenu from './UserMenu';
 
 function ResponsiveAppBar({ onLogout }) {
-  const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const {user} = useContext(UserContext);
   const {setErrors} = useContext(ErrorContext);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
@@ -38,7 +31,6 @@ function ResponsiveAppBar({ onLogout }) {
   };
 
   const handleLogout = () => {
-    // navigate("/");
     fetch("/logout", {
       method: 'DELETE'
     })
@@ -93,59 +85,8 @@ function ResponsiveAppBar({ onLogout }) {
 
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
             {user 
-            ?
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open user menu">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt={user.username} src={user.avatar_url} />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ 
-                  mt: '45px',
-                }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-                >
-                <MenuItem disableRipple sx={{
-                  width: '11rem', 
-                  cursor: 'default',
-                  '&:hover': {
-                    background: 'none',
-                  },
-                  justifyContent: 'center'
-                }} key={"Username"} >
-                  <Typography textAlign="center" sx={{ fontWeight: 'bold' }}>{user.username}</Typography>
-                </MenuItem>
-                <Divider />
-                <MenuItem sx={{ justifyContent:'center' }} key={"Profile"} onClick={handleCloseUserMenu} component={ Link } to='/profile'>
-                  <Typography textAlign="center">Profile</Typography>
-                </MenuItem>
-                <MenuItem sx={{ justifyContent:'center' }} key={"My Travelogues"} onClick={handleCloseUserMenu} component={ Link } to='/travelogues'>
-                  <Typography textAlign="center">Travelogues</Typography>
-                </MenuItem>
-                <MenuItem sx={{ justifyContent:'center' }} key={"Collections"} onClick={handleCloseUserMenu} component={ Link } to='/bookmarks'>
-                  <Typography textAlign="center">Bookmarks</Typography>
-                </MenuItem>
-                <Divider />
-                <MenuItem sx={{ justifyContent:'center' }} key={"Log out"} onClick={handleLogout}>
-                  <Typography textAlign="center">Log out</Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
-            :
-            <Button sx={{ flexGrow: 0 }} component={ Link } to='/login' onClick={() => setErrors(null)}>
+            ? <UserMenu user={user} handleLogout={handleLogout} anchorElUser={anchorElUser} handleCloseUserMenu={handleCloseUserMenu} handleOpenUserMenu={handleOpenUserMenu} />
+            : <Button sx={{ flexGrow: 0 }} component={ Link } to='/login' onClick={() => setErrors(null)}>
                 <Typography 
                   textAlign="center"
                   sx={{
---
+++ client/src/components/UserMenu.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+    Avatar,
+    Box,
+    Divider,
+    IconButton,
+    Menu,
+    MenuItem,
+    Tooltip,
+    Typography
+} from '@mui/material';
+
+const UserMenu = ({ 
+    user,
+    handleLogout,
+    anchorElUser, 
+    handleCloseUserMenu, 
+    handleOpenUserMenu  
+    }) => {
+
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open user menu">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt={user.username} src={user.avatar_url} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ 
+                  mt: '45px',
+                }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                >
+                <MenuItem disableRipple sx={{
+                  width: '11rem', 
+                  cursor: 'default',
+                  '&:hover': {
+                    background: 'none',
+                  },
+                  justifyContent: 'center'
+                }} key={"Username"} >
+                  <Typography textAlign="center" sx={{ fontWeight: 'bold' }}>{user.username}</Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem sx={{ justifyContent:'center' }} key={"Profile"} onClick={handleCloseUserMenu} component={ Link } to='/profile'>
+                  <Typography textAlign="center">Profile</Typography>
+                </MenuItem>
+                <MenuItem sx={{ justifyContent:'center' }} key={"My Travelogues"} onClick={handleCloseUserMenu} component={ Link } to='/travelogues'>
+                  <Typography textAlign="center">Travelogues</Typography>
+                </MenuItem>
+                <MenuItem sx={{ justifyContent:'center' }} key={"Collections"} onClick={handleCloseUserMenu} component={ Link } to='/bookmarks'>
+                  <Typography textAlign="center">Bookmarks</Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem sx={{ justifyContent:'center' }} key={"Log out"} onClick={handleLogout}>
+                  <Typography textAlign="center">Log out</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+  )
+}
+
+export default UserMenu
