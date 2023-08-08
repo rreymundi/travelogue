49498a06c258f5e6dd357f7d26f5f872249539fd
Raúl <raul@kickstarter.com>
2023-08-08 19:33:24 -0400

    create follows table and follow model

--- db/migrate/20230808014420_create_follows.rb
+++ db/migrate/20230808014420_create_follows.rb
@@ -2,7 +2,7 @@ class CreateFollows < ActiveRecord::Migration[6.1]
   def change
     create_table :follows do |t|
       t.integer :follower_id
-      t.integer :followed_id
+      t.integer :followed_user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_14_230942) do
+ActiveRecord::Schema.define(version: 2023_08_08_014420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2023_05_14_230942) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_tags", force: :cascade do |t|
