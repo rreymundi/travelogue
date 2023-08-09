bc6b8fa97e9ed93858c66ccbd451d392b866315a
Raúl <raul@kickstarter.com>
2023-08-08 22:20:55 -0400

    fix typo in the follow model associations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,8 +1,8 @@
 class Follow < ApplicationRecord
     # The user that is following
-    belongs_to :follower, foreign_key: :follower_id, class_name "User"
+    belongs_to :follower, foreign_key: :follower_id, class_name: "User"
 
     # The user that is being followed
-    belongs_to :followed_user, foreign_key: :followed_user_id, class_name "User"
+    belongs_to :followed_user, foreign_key: :followed_user_id, class_name: "User"
 
 end
