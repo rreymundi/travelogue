ccad8edd7a5def037acafaea32a0a9bc19fd02e1
Raúl <raul@kickstarter.com>
2023-08-08 22:14:58 -0400

    add associations in the user model in order to find a users followed users aka followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,12 @@ class User < ApplicationRecord
     validate :custom_password_validation, on: :create
     # Will return an array of follows for the given user instance
     has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
-    #Will return an array of users who follow the user instance
+    # Will return an array of users who follow the user instance
     has_many :followers, through: :received_follows, source: :follower
+    # Will return an array of follows a user gave to someone else
+    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+    # Will return an array of other users who the user has followed
+    has_many :followings, through: :given_follows, source: :followed_user
 
     def avatar_url
       if avatar.attached?
