c51012a970d3fb58af88aeaf72ba0131a0fb2c02
Raúl <raul@kickstarter.com>
2023-08-18 13:12:37 -0400

    add stateful follow button to the travelogue component that handles follow and unfollow fetch requests

--- client/src/pages/Travelogue.js
+++ client/src/pages/Travelogue.js
@@ -4,15 +4,18 @@ import { TravelogueContext } from '../context/travelogue';
 import { 
     Avatar,
     Box, 
+    Button, 
     Chip,
     Paper,
     Typography 
   } from '@mui/material';
+import { UserContext } from '../context/user';
 import parse from 'html-react-parser';
 import LoadingSpinner from '../components/LoadingSpinner';
 
-  const Travelogue = () => {
+  const Travelogue = ({ onFollowClick, onUnfollowClick }) => {
     const {travelogue, setTravelogue} = useContext(TravelogueContext);
+    const { user } = useContext(UserContext);
     const { id } = useParams();
     const [isLoading, setIsLoading] = React.useState(true);
     
@@ -37,9 +40,27 @@ import LoadingSpinner from '../components/LoadingSpinner';
       fetchData();
     }, [setTravelogue, id]);
 
+    const handleFollowClick = (e) => {
+      onFollowClick(travelogue.user)
+    };
+    
+    const handleUnfollowClick = (e) => {
+      onUnfollowClick(travelogue.user)
+    };
+
     // this variable makes use of the html-react-parser library to 
     // parse the travelogue description from HTML to JSX
   const renderedDescription = parse(`${travelogue.description}`)
+
+  const renderedFollowButton = user?.active_follows?.find(follow =>
+    follow.followed_user_id === travelogue?.user?.id)
+    ? <Button variant='contained' size="small" onClick={handleUnfollowClick}>
+        Unfollow
+      </Button>
+    : <Button variant='outlined' size="small" onClick={handleFollowClick}>
+        Follow
+      </Button>
+
 
   if (isLoading) return <LoadingSpinner />;
 
@@ -77,11 +98,12 @@ import LoadingSpinner from '../components/LoadingSpinner';
           >
           <Box xs={9} sx={{ display: 'flex', flexDirection: 'row' }}>
             <Typography variant='h4' sx={{ fontWeight: 'bold'}}>{travelogue?.title}</Typography>
-            <Box sx={{ ml: 'auto' }}>
-              <Avatar sx={{ height: '30px', width: '30px', mr: 'auto', ml: 'auto' }} alt={travelogue?.user?.username} src={travelogue?.user?.avatar_url} />
+            <Box sx={{ ml: 'auto', textAlign: 'center' }}>
+              <Avatar sx={{ height: '40px', width: '40px', mr: 'auto', ml: 'auto' }} alt={travelogue?.user?.username} src={travelogue?.user?.avatar_url} />
               <Typography gutterBottom variant="body2" color="text.secondary" >
                 {travelogue?.user?.username}
               </Typography>
+              {renderedFollowButton}
             </Box>
           </Box>
           <Box xs={12} sx={{ mb: '1rem' }}>
