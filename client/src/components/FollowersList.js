1919693f33773e45066b74a4c4533275df23dace
Raúl <raul@kickstarter.com>
2023-08-20 10:41:08 -0400

    add followerslist component

---
+++ client/src/components/FollowersList.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    Grid,
+    Typography,
+    List,
+    ListItem,
+    ListItemAvatar,
+    Avatar,
+    ListItemText
+} from '@mui/material';
+
+const FollowersList = ({ followers, following, onFollowClick, onUnfollowClick }) => {
+    const [dense, setDense] = useState(false);
+    const [secondary, setSecondary] = useState(false);
+
+    const renderedActiveFollows = followers?.map((follower) =>
+        <ListItem
+            key={follower.id}
+            secondaryAction={
+                following?.find(follow => follow.id === follower.id) 
+                ?   <Button variant='contained' size="small" onClick={() => onUnfollowClick(follower)} >
+                        Unfollow
+                    </Button>
+                :   <Button variant='outlined' size="small" onClick={() => onFollowClick(follower)} >
+                        Follow
+                    </Button>
+            }
+        >
+            <ListItemAvatar>
+                <Avatar alt={follower.username} src={follower.avatar_url} />
+            </ListItemAvatar>
+            <ListItemText
+                primary={follower.username}
+                secondary={secondary ? 'Secondary text' : null}
+            />
+        </ListItem>
+    );
+
+  return (
+    <Grid item xs={12} md={6}>
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Followers
+        </Typography>
+        <List dense={dense}>
+            {renderedActiveFollows}
+        </List>
+    </Grid>
+  )
+}
+
+export default FollowersList
