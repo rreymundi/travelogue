d40017bafd63d3a0e00e97bca04ca1cdc28a564d
Raúl <raul@kickstarter.com>
2023-09-22 11:13:58 -0400

    add cover_image validation to the travelogue model to make cover images required

--- app/models/travelogue.rb
+++ app/models/travelogue.rb
@@ -2,6 +2,7 @@ class Travelogue < ApplicationRecord
     has_one_attached :cover_image
     validates :title, presence: true
     validates :description, presence: true
+    validates :cover_image, presence: true
     belongs_to :user
     has_many :post_tags, dependent: :destroy
     has_many :tags, through: :post_tags
