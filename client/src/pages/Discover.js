d65e5fba7987beb5d5db1cd00024798323f4c680
Raúl <raul@kickstarter.com>
2023-09-27 19:15:56 -0400

    set up pagination in the Discover component minus dynamic URL params

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useRef } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Box,
     Button,
     Grid, 
@@ -6,17 +6,17 @@ import { Box,
 } from '@mui/material';
 import TravelogueCard from '../components/TravelogueCard';
 import { ErrorContext } from '../context/error';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import Search from '../components/Search';
 
-const Discover = ({ onBookmarkSave, onBookmarkUnsave }) => {
+const Discover = ({ onBookmarkSave, onBookmarkUnsave, handleSearch, setTotalPages, totalPages, data, setData, page, setPage, query, setQuery }) => {
     const {setErrors} = useContext(ErrorContext);
-    const [query, setQuery] = useState('');
+    // const [query, setQuery] = useState('');
     let [searchParams, setSearchParams] = useSearchParams();
     const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
+    // const [data, setData] = useState([]);
+    // const [page, setPage] = useState(1);
+    // const [totalPages, setTotalPages] = useState(0);
     
     const handleChange = (e) => {
       setQuery(e.target.value)
@@ -36,21 +36,31 @@ const Discover = ({ onBookmarkSave, onBookmarkUnsave }) => {
         setIsLoading(false);
       }
       fetchData();
-    }, [page]);
+    }, [page, setData, setErrors, setTotalPages]);
 
     // this fetch request is for the Search component in the Discover page
-    const handleSearch = (e) => {
+    // const handleSearch = (e) => {
+    //   e.preventDefault();
+    //   setSearchParams({'query': query})
+    //   fetch(`/discover/${page}/${query}`)
+    //   .then((r) => {
+    //     if (r.ok) {
+    //       r.json().then((data) => setData(data.travelogues))
+    //       setTotalPages(data.total_pages)
+    //     } else {
+    //       r.json().then((data) => setErrors(data.errors))
+    //     }
+    //   })
+    // };
+
+    const onSearch = (e) => {
       e.preventDefault();
-      setSearchParams({'query': query})
-      fetch(`/discover/${page}/${query}`)
-      .then((r) => {
-        if (r.ok) {
-          r.json().then((data) => setData(data.travelogues))
-        } else {
-          r.json().then((data) => setErrors(data.errors))
-        }
-      })
+      handleSearch(query)
     };
+
+    const onLoadMore = () => {
+      setPage((prev) => prev + 1)
+    }
 
     return (
       <Box sx={{
@@ -66,7 +76,7 @@ const Discover = ({ onBookmarkSave, onBookmarkUnsave }) => {
                     Browse through the latest travelogues
                 </Typography>
                 <Box sx={{ textAlign: 'center', m: '1rem' }}>
-                  <Search handleChange={handleChange} handleSearch={handleSearch} />
+                  <Search handleChange={handleChange} handleSearch={onSearch} />
                 </Box>
                 <Box sx={{ margin: '2.5rem'}}>
                   <Grid container spacing={2}>
@@ -80,7 +90,7 @@ const Discover = ({ onBookmarkSave, onBookmarkUnsave }) => {
                 <Box sx={{ textAlign: 'center', m: '1rem' }}>
                 {page < totalPages && (<Button
                   variant="contained"
-                  onClick={() => setPage((prev) => prev + 1)}
+                  onClick={onLoadMore}
                   >
                   {isLoading ? "Loading..." : "Load More"}
                   </Button>)}
