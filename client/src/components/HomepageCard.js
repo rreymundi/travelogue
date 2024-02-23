f15f50df11ab1cbb68b00dc450c5e6e77df01a9c
Raúl <raul@kickstarter.com>
2024-02-22 21:16:18 -0500

    update plainString regex to handle &nbsp and clean up component

--- client/src/components/HomepageCard.js
+++ client/src/components/HomepageCard.js
@@ -1,55 +1,59 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { 
-    Card,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Button,
-    Typography
-} from '@mui/material'
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import { UserContext } from '../context/user';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import { UserContext } from "../context/user";
 
 const HomepageCard = ({ travelogue, onBookmarkSave, onBookmarkUnsave }) => {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
   const handleSaveClick = () => {
-    onBookmarkSave(travelogue.id)
+    onBookmarkSave(travelogue.id);
   };
 
   const handleUnsaveClick = () => {
-    onBookmarkUnsave(travelogue.id)
+    onBookmarkUnsave(travelogue.id);
   };
 
   const handleClick = () => {
-    navigate(`/travelogues/${travelogue.id}`)
+    navigate(`/travelogues/${travelogue.id}`);
   };
 
   // this checks user.saved_posts to see if the travelogue is saved by the user and renders the appropriate icon
-  const renderedBookmarkButton = user?.saved_posts.find(post => 
-    post.travelogue_id === travelogue.id) 
-    ? <BookmarkIcon onClick={handleUnsaveClick} /> 
-    : <BookmarkBorderIcon onClick={handleSaveClick} />
+  const renderedBookmarkButton = user?.saved_posts.find(
+    (post) => post.travelogue_id === travelogue.id
+  ) ? (
+    <BookmarkIcon onClick={handleUnsaveClick} />
+  ) : (
+    <BookmarkBorderIcon onClick={handleSaveClick} />
+  );
 
   // this uses regex to strip html tags from the travelogue.description returned from the API
   const htmlString = `${travelogue.description}`;
-  const plainString = htmlString.replace(/(<([^>]+)>)/ig, '');
-
+  const plainString = htmlString.replace(/(<([^>]+)>)|(&nbsp;)/gi, "");
+  
   return (
-    <Card sx={{ 
-        width: '250px', 
-        margin: '1rem', 
-        display: 'flex', 
-        flexDirection: 'column',
-        '&:hover': {
-          transform: 'translateX(-2px) translateY(-2px)'
-          },
-        transition: 'all 450ms cubic-bezier(0.175, 0.885, 0.335, 1)',
-        }}
-      >
+    <Card
+      sx={{
+        width: "250px",
+        margin: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        "&:hover": {
+          transform: "translateX(-2px) translateY(-2px)",
+        },
+        transition: "all 450ms cubic-bezier(0.175, 0.885, 0.335, 1)",
+      }}
+    >
       <CardMedia
         component="img"
         alt="green iguana"
@@ -57,23 +61,21 @@ const HomepageCard = ({ travelogue, onBookmarkSave, onBookmarkUnsave }) => {
         image={travelogue.cover_image_url}
       />
       <CardContent>
-        <Typography gutterBottom component="div" sx={{ fontWeight: 'bold' }}>
+        <Typography gutterBottom component="div" sx={{ fontWeight: "bold" }}>
           {travelogue.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {plainString.slice(0, 50) + '...'}
+          {plainString.slice(0, 50) + "..."}
         </Typography>
       </CardContent>
-      <CardActions sx={{ justifyContent: 'space-between', marginTop: 'auto'}}>
-        <Button size="small" onClick={handleClick} >
+      <CardActions sx={{ justifyContent: "space-between", marginTop: "auto" }}>
+        <Button size="small" onClick={handleClick}>
           Read
         </Button>
-        <Button size="small" >
-            {renderedBookmarkButton}
-        </Button>
+        <Button size="small">{renderedBookmarkButton}</Button>
       </CardActions>
     </Card>
   );
-}
+};
 
-export default HomepageCard
+export default HomepageCard;
--- client/src/components/TravelogueCard.js
+++ client/src/components/TravelogueCard.js
@@ -1,107 +1,162 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import {
-  Avatar, 
+  Avatar,
   Box,
   Button,
   Card,
   CardMedia,
-  Typography
- } from '@mui/material';
- import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
- import BookmarkIcon from '@mui/icons-material/Bookmark';
- import { UserContext } from '../context/user';
+  Typography,
+} from "@mui/material";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import { UserContext } from "../context/user";
 
 const TravelogueCard = ({ travelogue, onBookmarkSave, onBookmarkUnsave }) => {
   const { user } = useContext(UserContext);
   let navigate = useNavigate();
 
-  const publishedDate = new Date(travelogue.created_at).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+  const publishedDate = new Date(travelogue.created_at).toLocaleDateString(
+    "en-US",
+    { year: "numeric", month: "long", day: "numeric" }
+  );
 
   const handleClick = () => {
-    navigate(`/travelogues/${travelogue.id}`)
+    navigate(`/travelogues/${travelogue.id}`);
   };
 
   const handleSaveClick = (e) => {
-    onBookmarkSave(travelogue.id)
+    onBookmarkSave(travelogue.id);
   };
 
   const handleUnsaveClick = (e) => {
-    onBookmarkUnsave(travelogue.id)
+    onBookmarkUnsave(travelogue.id);
   };
 
   // this checks user.saved_posts to see if the travelogue is saved by the user and renders the appropriate icon
-  const renderedBookmarkButton = user?.saved_posts.find(post => 
-    post.travelogue_id === travelogue.id) 
-    ? <BookmarkIcon onClick={handleUnsaveClick} /> 
-    : <BookmarkBorderIcon onClick={handleSaveClick} />;
+  const renderedBookmarkButton = user?.saved_posts.find(
+    (post) => post.travelogue_id === travelogue.id
+  ) ? (
+    <BookmarkIcon onClick={handleUnsaveClick} />
+  ) : (
+    <BookmarkBorderIcon onClick={handleSaveClick} />
+  );
 
   // this uses regex to strip html tags from the travelogue.description returned from the API
   const htmlString = `${travelogue.description}`;
-  const plainString = htmlString.replace(/(<([^>]+)>)/ig, '');  
+  const plainString = htmlString.replace(/(<([^>]+)>)|(&nbsp;)/gi, "");
   
   return (
-    <Card sx={{ 
-        width:'100%', 
-        heigh: '100%', 
-        margin: '1rem',
-        '&:hover': {
-            transform: 'translateX(-2px) translateY(-2px)'
-            },
-          transition: 'all 450ms cubic-bezier(0.175, 0.885, 0.335, 1)', 
+    <Card
+      sx={{
+        width: "100%",
+        heigh: "100%",
+        margin: "1rem",
+        "&:hover": {
+          transform: "translateX(-2px) translateY(-2px)",
+        },
+        transition: "all 450ms cubic-bezier(0.175, 0.885, 0.335, 1)",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          boxSizing: "border-box",
         }}
-        >
-        <Box sx={{ display: 'flex', alignItems:'center', justifyContent:'space-between', boxSizing: 'border-box' }}>
-          <Box sx={{ width:'100%', minWidth: '0', padding: '1rem'}} >
-            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-              <Avatar sx={{ height: '30px', width: '30px' }} alt={travelogue?.user?.username} src={travelogue?.user?.avatar_url} />
-              <Typography gutterBottom variant="body2" color="text.secondary" sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', ml: '1rem' }}>
-                {travelogue?.user?.username}
-              </Typography>
-            </Box>
-            <Box onClick={handleClick} sx={{ cursor: 'pointer'}}>
-              <Typography gutterBottom variant="h5" component="div" sx={{ maxHeight: 'none'}}>
-                {travelogue.title}
-              </Typography>
-              <Typography variant="body2" color="primary" >
-                {travelogue.location}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" sx={{ paddingTop: '4px' }}>
-                {plainString.slice(0, 70) + '...'}
-              </Typography>
-            </Box>
-            <Box sx={{ justifyContent: 'space-between', display: 'flex', alignItems: 'center' }} >
-              <Box sx={{ display: 'flex' }}>
-                <Typography variant="body2" color="text.secondary" sx={{ mr: '0.375rem' }}>
-                  {publishedDate}
-                </Typography>
-                {travelogue?.tags?.map((tag) => 
-                  <Typography key={tag.id} variant="body2" color="primary" sx={{ ml: '0.375rem' }}>
-                    #{tag.name}
-                  </Typography>
-                )}
-              </Box>
-              <Box sx={{ display:'flex' }} >
-                <Button size="small">
-                  {renderedBookmarkButton}
-                </Button>
-              </Box>
-            </Box>
+      >
+        <Box sx={{ width: "100%", minWidth: "0", padding: "1rem" }}>
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Avatar
+              sx={{ height: "30px", width: "30px" }}
+              alt={travelogue?.user?.username}
+              src={travelogue?.user?.avatar_url}
+            />
+            <Typography
+              gutterBottom
+              variant="body2"
+              color="text.secondary"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                ml: "1rem",
+              }}
+            >
+              {travelogue?.user?.username}
+            </Typography>
           </Box>
-          <Box>
-            { travelogue.cover_image_url !== null 
-              ?
-              <CardMedia
-                  sx={{ verticalAlign: 'middle', width: '275px', aspectRatio: 'auto 200 / 134 ', height: '100%', cursor: 'pointer' }}
-                  image={travelogue.cover_image_url}
-                  title="travelogue image"
-                  onClick={handleClick}
-              />
-              : null }
+          <Box onClick={handleClick} sx={{ cursor: "pointer" }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ maxHeight: "none" }}
+            >
+              {travelogue.title}
+            </Typography>
+            <Typography variant="body2" color="primary">
+              {travelogue.location}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ paddingTop: "4px" }}
+            >
+              {plainString.slice(0, 70) + "..."}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              justifyContent: "space-between",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ display: "flex" }}>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ mr: "0.375rem" }}
+              >
+                {publishedDate}
+              </Typography>
+              {travelogue?.tags?.map((tag) => (
+                <Typography
+                  key={tag.id}
+                  variant="body2"
+                  color="primary"
+                  sx={{ ml: "0.375rem" }}
+                >
+                  #{tag.name}
+                </Typography>
+              ))}
+            </Box>
+            <Box sx={{ display: "flex" }}>
+              <Button size="small">{renderedBookmarkButton}</Button>
+            </Box>
           </Box>
         </Box>
+        <Box>
+          {travelogue.cover_image_url !== null ? (
+            <CardMedia
+              sx={{
+                verticalAlign: "middle",
+                width: "275px",
+                aspectRatio: "auto 200 / 134 ",
+                height: "100%",
+                cursor: "pointer",
+              }}
+              image={travelogue.cover_image_url}
+              title="travelogue image"
+              onClick={handleClick}
+            />
+          ) : null}
+        </Box>
+      </Box>
     </Card>
   );
-}
+};
 
-export default TravelogueCard
+export default TravelogueCard;
